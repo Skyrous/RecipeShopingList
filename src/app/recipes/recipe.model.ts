7613c8727aae7382a89ce87988d412639097167b
Skyrous <balan.claudiu96@gmail.com>
2023-03-16 12:03:00 +0200

    -

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,7 +1,26 @@
 export class Recipe {
-  public nameOfRecipe: string;
-  public description: string;
-  public imagePath: string;
+  private nameOfRecipe: string;
+  public getNameOfRecipe(): string {
+    return this.nameOfRecipe;
+  }
+  public setNameOfRecipe(value: string) {
+    this.nameOfRecipe = value;
+  }
+  private description: string;
+  public getDescription(): string {
+    return this.description;
+  }
+  public setDescription(value: string) {
+    this.description = value;
+  }
+
+  private imagePath: string;
+  public getImagePath(): string {
+    return this.imagePath;
+  }
+  public setImagePath(value: string) {
+    this.imagePath = value;
+  }
   constructor(nameOfRecipe: string, description: string, imagePath: string) {
     this.nameOfRecipe = nameOfRecipe;
     this.description = description;
